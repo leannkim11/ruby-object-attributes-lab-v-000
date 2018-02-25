31ea82a82ac1348e9ec82e42b0ada235f47608e4
Leann Kim <leannminakim@gmail.com>
2018-02-25 19:26:06 +0000

    automatically backed up by learn

---
+++ lib/person.rb
@@ -0,0 +1,26 @@
+class Person
+  def person_name(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def name= (new_name)
+    @name
+  end
+
+  def person_job(job)
+    @job = job
+  end
+
+  def job
+    @job
+  end
+
+  def job= (new_job)
+    @job
+  end
+  
+end
